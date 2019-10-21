d715be9c062d3e56e26ef40faec6d928e7cc5b73
szkmiyabi <led.centipede@gmail.com>
2019-10-22 02:00:54 +0900

    リスト要素マークアップ、別ウィンドウ明示の挙動修正

--- src/textUtil.js
+++ src/textUtil.js
@@ -28,7 +28,6 @@ module.exports = class textUtil {
             line = line.replace(/(<\/*p.*?>|<\/*div.*?>)/g, "");
             new_txt += `<${cr_tag}>${line}</${cr_tag}>` + "\n";
         }
-        //this.editor.setValue(old_txt + "\n\n" + new_txt);
         this.editor.session.replace(range, old_txt + "\n\n" + new_txt);
     }
 
@@ -46,7 +45,7 @@ module.exports = class textUtil {
         let new_txt = "";
         let range = this.editor.getSelectionRange();
         let txt = this.editor.session.getTextRange();
-        let old_txt = this.editor.getValue();
+        let old_txt = this.editor.session.getTextRange();
         txt = txt.replace(/(\r\n|\n|\r){2}/, "\n");
         let start_pt = new RegExp(/(>>ul\n|>>ol\n|>>dl\n)/m);
         let end_pt = new RegExp(/(\n>>\/ul|\n>>\/ol|\n>>\/dl)/m);
@@ -76,7 +75,7 @@ module.exports = class textUtil {
         } else if(cr_tag === "ol") {
             new_txt = "<ol>\n" + new_txt + "</ol>";
         }
-        this.editor.setValue(old_txt + "\n\n" + new_txt);
+        this.editor.session.replace(range, old_txt + "\n\n" + new_txt);
     }
 
     //別ウィンドウの明示自動修正
@@ -84,7 +83,7 @@ module.exports = class textUtil {
         let new_txt = "";
         let range = this.editor.getSelectionRange();
         let txt = this.editor.session.getTextRange();
-        let old_txt = this.editor.getValue();
+        let old_txt = this.editor.session.getTextRange();
         let datas = txt.split(/\n/);
         for(var i=0; i<datas.length; i++) {
             var type = "";
@@ -102,7 +101,7 @@ module.exports = class textUtil {
                 new_txt += row + "\n";
             }
         }
-        this.editor.setValue(old_txt + "\n\n" + new_txt);
+        this.editor.session.replace(range, old_txt + "\n\n" + new_txt);
     }
     _is_img_link(str) {
         var pt = new RegExp(/(<a.+?>)(.*?<img.+?>.*?)(<\/a>)/);
